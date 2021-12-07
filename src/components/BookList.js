1fb15d43850ed746036a37a0fe563a4f912422fd
Timowest12 <timowester@gmail.com>
2021-12-07 15:54:57 +0100

    Add reducer call

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,19 +1,45 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useState } from 'react';
 import Book from './Book';
+import addUser, { addBook } from '../redux/books/books';
+import { useSelector,useDispatch,connect } from 'react-redux';
+import books from '../redux/books/books';
+import { v4 as uuidv4 } from 'uuid';
 
-const BookList = (props) => {
-  const { bookList } = props;
+
+const BookList = () => {
+    const [booksstate, setbooksstate] = useState([]);
+    const dispatch = useDispatch();
+  
+  const submitForm = (e) => {
+      e.preventDefault();
+    const title = e.target.querySelector('#title')
+    const author = e.target.querySelector('#author')
+    const newbook = {id:uuidv4(),title:title.value,author:author.value};
+    title.value = '';
+    author.value = '';
+    dispatch(addBook(newbook));
+    setbooksstate('hello')
+  }
+  const bookList = useSelector((state) => state.books)
+  
+  
   return (
     <div>
+        {console.log(bookList)}
+        {console.log(booksstate)}
       <h1>Awesome Books</h1>
+      
       <ul>
+
         {bookList.map((book) => (
-          <Book key={book.id} book={book} />
+          console.log(book)
         ))}
       </ul>
-      <form id="add-book">
-        <input placeholder="Book Title" />
-        <input placeholder="Book Author" />
+      <form id="add-book-form" onSubmit={(e) => submitForm(e)}>
+
+        <input id='title' placeholder="Title" />
+        <input id='author' placeholder="Author" />
         <button type="submit">Add Book</button>
       </form>
     </div>
