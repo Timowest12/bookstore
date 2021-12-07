cd448564295bcd7753992f51fb2e51192804d5e9
Timowest12 <timowester@gmail.com>
2021-12-07 08:47:46 +0100

    Setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import { React } from 'react';
 import './App.css';
+import BookList from './components/BookList';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+const demoList = [{
+    id: 1, title: 'book1', author: 'author1',
+  }, {
+    id: 2, title: 'book2', author: 'author2',
+  }, {
+    id: 3, title: 'book3', author: 'author3',
+  }];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        <nav>
+          <h1>Awesome Bookstore</h1>
+          <div className="nav-links">
+            <Link to="/">Book</Link>
+            <div />
+            <Link to="/categories">Categories</Link>
+          </div>
+        </nav>
       </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<BookList bookList={demoList} />} />
+        {/* <Route path="/categories" element={<CategoriesPage />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
